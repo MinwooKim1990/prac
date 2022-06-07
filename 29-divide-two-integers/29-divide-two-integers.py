c209a0b5dc11377a89e2c13da2fb489981a0658c
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2022-06-07 22:33:41 +0100

    Time: 253 ms (5.23%), Space: 32.1 MB (25.31%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -1,30 +1,26 @@
 import numpy
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        if -2**31 <= dividend <= 2**31-1 and -2**31 <= divisor <= 2**31-1 and divisor !=0 :
-            e=abs(dividend)
-            s=abs(divisor)
-            i = numpy.exp(numpy.log(e)-numpy.log(s))
-            print(i)
-            if dividend<0 and divisor>0:
-              i=numpy.int(numpy.ceil(-i))
-            elif dividend>0 and divisor<0:
-              i=numpy.int(numpy.ceil(-i))
-            else:
-              i=numpy.int(numpy.round(i))
-            if i > 2**31-1:
-                i=2**31-1
-            elif i < -2**31:
-                i=-2**31
-            if abs(numpy.round(numpy.exp(numpy.log(i)+numpy.log(s))))>abs(dividend):
-              i=i-1
-            return i
-        elif -2**31 > dividend or -2**31 > divisor:
-            return -2**31
-        elif 2**31-1 < dividend or 2**31-1 < divisor:
-            return 2**31-1
-        else:
-            return 0
+          e=abs(dividend)
+          s=abs(divisor)
+          i = numpy.exp(numpy.log(e)-numpy.log(s))
+          print(i)
+          if dividend<0 and divisor>0:
+            i=numpy.int(numpy.ceil(-i))
+          elif dividend>0 and divisor<0:
+            i=numpy.int(numpy.ceil(-i))
+          else:
+            i=numpy.int(numpy.round(i))
+          if i > 2**31-1:
+              i=2**31-1
+          elif i < -2**31:
+              i=-2**31
+          if abs(numpy.round(numpy.exp(numpy.log(i)+numpy.log(s))))>abs(dividend):
+            i=i-1
+          return i
+if __name__ == '__main__':
+    # begin
+    print(Solution().divide(-1021418889,1064910933))
         
 if __name__ == '__main__':
     # begin
