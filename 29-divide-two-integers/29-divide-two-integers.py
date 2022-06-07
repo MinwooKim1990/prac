175d11bea749c74e2d2c63741891c96e783506dc
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2022-06-07 22:34:47 +0100

    Time: 266 ms (5.23%), Space: 31.9 MB (25.31%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -1,23 +1,22 @@
 import numpy
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-          e=abs(dividend)
-          s=abs(divisor)
-          i = numpy.exp(numpy.log(e)-numpy.log(s))
-          print(i)
-          if dividend<0 and divisor>0:
+        e=abs(dividend)
+        s=abs(divisor)
+        i = numpy.exp(numpy.log(e)-numpy.log(s))
+        if dividend<0 and divisor>0:
             i=numpy.int(numpy.ceil(-i))
-          elif dividend>0 and divisor<0:
+        elif dividend>0 and divisor<0:
             i=numpy.int(numpy.ceil(-i))
-          else:
+        else:
             i=numpy.int(numpy.round(i))
-          if i > 2**31-1:
-              i=2**31-1
-          elif i < -2**31:
-              i=-2**31
-          if abs(numpy.round(numpy.exp(numpy.log(i)+numpy.log(s))))>abs(dividend):
+        if i > 2**31-1:
+            i=2**31-1
+        elif i < -2**31:
+            i=-2**31
+        if abs(numpy.round(numpy.exp(numpy.log(i)+numpy.log(s))))>abs(dividend):
             i=i-1
-          return i
+        return i
 if __name__ == '__main__':
     # begin
     print(Solution().divide(-1021418889,1064910933))
