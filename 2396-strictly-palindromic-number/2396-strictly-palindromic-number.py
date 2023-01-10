bede5748430da96678b04873a797b71b92cb4dd5
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2023-01-10 18:18:47 +0000

    Time: 35 ms (74.82%), Space: 13.9 MB (11.58%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,3 +1,13 @@
 class Solution:
+    def solution(n, q):
+        rev_base = ''
+
+        while n > 0:
+            n, mod = divmod(n, q)
+            rev_base += str(mod)
+
+        return rev_base[::-1] 
+    # 역순인 진수를 뒤집어 줘야 원래 변환 하고자하는 base가 출력
+
     def isStrictlyPalindromic(self, n: int) -> bool:
         return False
