51a2172ed945c50bf7400ec2ade1aa67b4011fc1
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2023-01-14 12:18:12 +0000

    Time: 236 ms (5.51%), Space: 15 MB (35.06%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,12 @@
-import math
-import itertools
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-#        res=[]
-#        for r in range(len(nums)):
-#            for i in #range(math.factorial(len(nums))/(math.factorial(r+1)*math.factorial(len(nums)-#(r+1)))):
-#                temp=[nums[i]]
-#                res.append(temp)
-        res=[]
-        for x in range(len(set(nums))+1):
-            res+=list(combinations(set(nums), x))
-        return res
+
+        result = [[]]
+
+        for n in nums:
+            print('n',n)
+            print('res', result)
+            for i in range(len(result)):
+                result.append(result[i]+[n])
+                print('result',result)
+        return result
