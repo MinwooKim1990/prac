a352876b0f176db1116d6b859a3337eea003159c
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2023-01-25 17:43:22 +0000

    Time: 3098 ms (5.35%), Space: 27.9 MB (83.93%) - LeetHub

---
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shortestSequence(self, rolls, k):
+        res = 1
+        s = set()
+        for a in rolls:
+            s.add(a)
+            if len(s) == k:
+                res += 1
+                s.clear()
+        return res
