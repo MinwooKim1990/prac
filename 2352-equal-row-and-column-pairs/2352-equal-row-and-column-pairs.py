7a18d03861d36acd86987ee54fac243fc30525ef
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2023-01-27 21:21:16 +0000

    Time: 2599 ms (10.85%), Space: 38.4 MB (5.30%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -0,0 +1,13 @@
+import numpy as np
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        count=0
+        grid1=np.asmatrix(grid)
+        grid2=np.asmatrix(grid).T
+        for i in range(len(grid)):    
+            grid3=len(grid)-np.count_nonzero(np.sum(abs(grid2-np.tile(np.array(grid1)[i], (len(grid),1))),axis=1))
+            print(np.sum(abs(grid2-np.tile(np.array(grid1)[i], (len(grid),1))),axis=1))
+            print(grid3)
+            count+= grid3
+        return count
+                    
