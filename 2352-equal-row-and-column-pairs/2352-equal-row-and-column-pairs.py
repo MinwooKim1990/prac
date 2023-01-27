d333688cefb216dd919dbb89a8869c492f3e46f3
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2023-01-27 21:21:33 +0000

    Time: 1355 ms (34.24%), Space: 38.1 MB (5.30%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -6,8 +6,6 @@ class Solution:
         grid2=np.asmatrix(grid).T
         for i in range(len(grid)):    
             grid3=len(grid)-np.count_nonzero(np.sum(abs(grid2-np.tile(np.array(grid1)[i], (len(grid),1))),axis=1))
-            print(np.sum(abs(grid2-np.tile(np.array(grid1)[i], (len(grid),1))),axis=1))
-            print(grid3)
             count+= grid3
         return count
                     
