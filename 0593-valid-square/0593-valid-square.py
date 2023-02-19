895c123a0ef402a84a3f3ac2621b773772697421
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2023-02-19 14:29:20 +0000

    Time: 94 ms (5.17%), Space: 31.8 MB (5.39%) - LeetHub

--- 0593-valid-square/0593-valid-square.py
+++ 0593-valid-square/0593-valid-square.py
@@ -9,12 +9,12 @@ class Solution:
         
         dot_products = [[np.dot(np.array(pp[i-3])-np.array(pp[i]),np.array(pp[i-2])-np.array(pp[i])),
                         np.dot(np.array(pp[i-3])-np.array(pp[i]),np.array(pp[i-1])-np.array(pp[i])),
-                        np.dot(np.array(pp[i-2])-np.array(pp[i]),np.array(pp[i-1])-np.array(pp[i]))] for i in range(4)]
+                        np.dot(np.array(pp[i-2])-np.array(pp[i]),np.array(pp[i-1])-np.array(pp[i]))] for i in range(2)]
         
-        for i in range(4):
+        for i in range(2):
             res.append(sorted(dot_products[i]))
             
-        if res[0][1]==res[0][2]==res[1][1]==res[1][2]==res[2][1]==res[2][2]==res[3][1]==res[3][2]:
+        if res[0][1]==res[0][2]==res[1][1]==res[1][2]:
             return True
         
         return False
