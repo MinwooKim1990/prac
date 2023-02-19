925928de5d00ee0b91304db9f5bac14af92e59fe
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2023-02-19 14:32:06 +0000

    Time: 106 ms (5.17%), Space: 31.9 MB (5.39%) - LeetHub

--- 0593-valid-square/0593-valid-square.py
+++ 0593-valid-square/0593-valid-square.py
@@ -2,14 +2,14 @@ import numpy as np
 
 class Solution:
     def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
-        pp = [p1, p2, p3, p4]
+        pp = [np.array(p1), np.array(p2), np.array(p3), np.array(p4)]
         res = []
         if p1 == p2 == p3 == p4:
             return False
         
-        dot_products = [[np.dot(np.array(pp[i-3])-np.array(pp[i]),np.array(pp[i-2])-np.array(pp[i])),
-                        np.dot(np.array(pp[i-3])-np.array(pp[i]),np.array(pp[i-1])-np.array(pp[i])),
-                        np.dot(np.array(pp[i-2])-np.array(pp[i]),np.array(pp[i-1])-np.array(pp[i]))] for i in range(2)]
+        dot_products = [[np.dot(pp[i-3]-pp[i],pp[i-2]-pp[i]),
+                        np.dot(pp[i-3]-pp[i],pp[i-1]-pp[i]),
+                        np.dot(pp[i-2]-pp[i],pp[i-1]-pp[i])] for i in range(2)]
         
         for i in range(2):
             res.append(sorted(dot_products[i]))
