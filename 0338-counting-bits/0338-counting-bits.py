a89631fa33304590d9f452a781540f1cfaafe38f
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2024-02-27 19:53:39 +0900

    Time: 60 ms (75.22%), Space: 23.1 MB (90.49%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,7 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans=[bin(i)[2:] for i in range(n+1)]
-        ans2=[]
-        for i in ans:
-            ans2.append(sum(list(map(int,list(i)))))
-        return ans2
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
+        return ans
