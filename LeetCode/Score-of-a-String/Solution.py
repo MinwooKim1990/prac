714a43a3e04d12a93bc310f936df4276ee485f94
Minwoo Kim <67540090+MinwooKim1990@users.noreply.github.com>
2025-05-13 14:18:05 +0900

    Add solution for Score of a String

--- LeetCode/Score-of-a-String/Solution.py
+++ LeetCode/Score-of-a-String/Solution.py
@@ -1,4 +1,7 @@
 import numpy as np
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        return int(sum([abs(c) for c in np.array([ord(a) for a in s[:-1]])-np.array([ord(b) for b in s[1:]])]))
+        tot = 0
+        for i in range(len(s)-1):
+            tot+=abs(ord(s[i])-ord(s[i+1]))
+        return tot
